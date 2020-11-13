445af5e60f7fbdc80dc9c77793593a4b3386c8bc
Paraizo <felipeparaizo@gmail.com>
2020-11-13 00:17:15 -0300

    changes: requests to gif and recipe API

--- handler/recipe.go
+++ handler/recipe.go
@@ -1,11 +1,21 @@
 package handler
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"strings"
+
+	"github.com/paraizofelipe/gorecipes/api"
+	"github.com/paraizofelipe/gorecipes/model"
 )
 
-// RecipesHandler ---
+type Response struct {
+	Keywords []string       `json:"keywords"`
+	Recipes  []model.Recipe `json:"recipes"`
+}
+
+// RecipeHandler ---
 func (h Handler) RecipeHandler(w http.ResponseWriter, r *http.Request) {
 	router := NewRouter(h.Logger)
 	router.Add(`recipes\/?$`, http.MethodGet, h.getRecipes())
@@ -13,14 +23,57 @@ func (h Handler) RecipeHandler(w http.ResponseWriter, r *http.Request) {
 	router.ServeHTTP(w, r)
 }
 
+func (h Handler) APIRecipeToRecipe(apiRecipes []model.APIRecipe) (recipes []model.Recipe, err error) {
+	var recipe model.Recipe
+	for _, apiRecipe := range apiRecipes {
+		recipe.Title = apiRecipe.Title
+		recipe.Link = apiRecipe.Href
+		recipe.Ingredients = strings.Split(apiRecipe.Ingredients, ",")
+		if recipe.Gif, err = api.SearchGif(recipe.Title); err != nil {
+			log.Println(err)
+			return
+		}
+		recipes = append(recipes, recipe)
+	}
+
+	return
+}
+
 func (h Handler) getRecipes() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var pathParams string
+		var (
+			err         error
+			recipes     []model.Recipe
+			response    Response
+			respRecipe  model.APIRecipeResponse
+			ingredients string
+		)
+
 		ctx := r.Context()
 		w.Header().Set("Content-Type", "application/json")
 
-		pathParams = ctx.Value("ingredients").(string)
-		log.Printf(" --- PARAMS %s", pathParams)
+		ingredients = r.URL.Query().Get("i")
+		if respRecipe, err = api.SearchRecipes(ingredients); err != nil {
+			log.Println(err)
+			http.Error(w, "error fetching recipe", http.StatusInternalServerError)
+			return
+		}
+		if len(respRecipe.Results) <= 0 {
+			log.Println(err)
+			http.Error(w, "recipes not found", http.StatusNotFound)
+			return
+		}
+		if recipes, err = h.APIRecipeToRecipe(respRecipe.Results); err != nil {
+			log.Println(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		response.Recipes = recipes
+
+		if err := json.NewEncoder(w).Encode(response); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 		w.WriteHeader(http.StatusOK)
 		r = r.WithContext(ctx)
 	}
